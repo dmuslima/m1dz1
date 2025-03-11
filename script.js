091d9508bb6c282e8edc8caf266e8e66a6ea6fd9
dmuslima <pirmukhamedovamuslima01@gmail.com>
2025-03-11 09:48:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let info = prompt('Ismingizni kiriting');
+console.log(info);
+
+let age = prompt('Yoshingizni kiriting');
+console.log(age);  
+
+let adres = prompt('Manzilingizni kiriting');
+console.log(adres);
+
+alert(`Mening ismim${info}, yoshim ${age}da, men ${adres} danman`);
